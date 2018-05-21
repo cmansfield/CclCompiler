666b0ae51b70663ed1e83c922270bfbb179b42dd
cmansfield <christopherjman@familysearch.org>
2018-05-21 16:46:22 -0600

    Started working on Symbol deserialization

--- src/main/java/io/github/cmansfield/io/CompilerReader.java
+++ src/main/java/io/github/cmansfield/io/CompilerReader.java
@@ -1,11 +1,15 @@
 package io.github.cmansfield.io;
 
 import io.github.cmansfield.secondpass.icode.IntermediateCode;
+import io.github.cmansfield.firstpass.symbols.Symbol;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
+
+import static io.github.cmansfield.io.IoConstants.EXPORT_DIR;
 
 
 public class CompilerReader {
@@ -17,11 +21,11 @@ public class CompilerReader {
    * @return            An IntermediateCode pojo
    */
   public static IntermediateCode importICode() throws IOException {
-    return importICode(IoConstants.I_CODE_FILE_NAME);    
+    return importICode(EXPORT_DIR + IoConstants.I_CODE_FILE_NAME);    
   }
 
   /**
-   *  This will create an IntermediateCode pojo from the supplied json file
+   * This will create an IntermediateCode pojo from the supplied json file
    * 
    * @param fileName    A file with iCode in json format
    * @return            An IntermediateCode pojo
@@ -35,5 +39,33 @@ public class CompilerReader {
     catch (Exception e) {
       throw new IOException("Unable to load file " + fileName, e);
     }
+  }
+
+  /**
+   * This will import a json format SymbolTable file from the default export location
+   * 
+   * @return            A Map containing all of the imported Symbols
+   */
+  public static Map<String, Symbol> importSymbolTable() throws IOException {
+    return importSymbolTable(EXPORT_DIR + IoConstants.SYMBOL_TABLE_FILE_NAME);
+  }
+  
+  /**
+   * This will create SymbolTable Map from the supplied json file
+   * 
+   * @param fileName    A file containing the symbol table in json format
+   * @return            A Map containing all of the imported Symbols
+   */
+  public static Map<String, Symbol> importSymbolTable(String fileName) throws IOException {
+    throw new UnsupportedOperationException("Need to implement a deserializer for the Symbol and Data classes first");
+    
+//    ObjectMapper mapper = new ObjectMapper();
+//
+//    try(InputStream inputstream = new FileInputStream(fileName)) {
+//      return mapper.readValue(inputstream, new TypeReference<Map<String, Symbol>>(){});
+//    }
+//    catch (Exception e) {
+//      throw new IOException("Unable to load file " + fileName, e);
+//    }
   }
 }
